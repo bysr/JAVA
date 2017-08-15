b501c8340c8f25a4fa74b559dacb1d7eed6edaec
wangyawen <1350212947@qq.com>
2017-08-15 10:44:37 +0800

    打印菱形图案

--- countlibs/src/main/java/com/example/Test19.java
+++ countlibs/src/main/java/com/example/Test19.java
@@ -14,21 +14,58 @@ public class Test19 {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        //绘制正三角 
-        for (int i = 1; i <= n; i++) {
-            System.out.println("");
-            for (int j = 0; j < 2 * i - 1; j++) {
+        //绘制正三角
+
+
+        for (int i = 0; i < n; i++) {
+
+            //1.先绘制空格，2.绘制*
+
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (i + 1) * 2 - 1; j++) {
                 System.out.print("*");
             }
+            System.out.println();
+
+
         }
+        //正三角
+
+        //倒三角
         for (int i = n - 1; i > 0; i--) {
-            System.out.println("");
-
-            for (int j = 0; j < 2 * i - 1; j++) {
+            for (int j = i - 1; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i * 2 - 1; k > 0; k--) {
                 System.out.print("*");
             }
+            System.out.println();
+        }
+
+
+        //打印平行四边形
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
 
         }
+
+
+
 
 
     }
